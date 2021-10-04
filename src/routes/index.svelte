6b25c6e6994dbc53092544cd43dc0ccf581715f1
Folke Sverdrup Nygren <91750365+folkesverdrup@users.noreply.github.com>
2021-10-04 15:18:20 +0200

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,19 +2,11 @@
     let routes = [
         {'url': 'seven', 'title': 'Seven sages laid its foundations'},
         {'url': 'sun', 'title': 'The sun hitches up her skirt for Utnapishtim'},
-        {'url': 'fuzzy', 'title': 'Fuzzy is the language of creation'},
-        {'url': 'garnitures', 'title': 'Garnitures for temple or house'},
-        {'url': 'haben', 'title': 'Haben sie bigbag (Ecstacy of Saint Sebastian)'},
-        {'url': 'lilt', 'title': 'Lilt Series'},
-        {'url': 's_90_3', 'title': 'S 90 3'},
-        {'url': '1UU', 'title': '1UU'},
-        {'url': 'other', 'title': 'other'},
-        {'url': 'contact', 'title': 'Contact'},
     ];
 </script>
 
 <main>
-    <h1>FOLKE</h1>
+    <h1>FOLKE SVERDRUP SAENGER</h1>
     {#each routes as route}
         <a href="/{route.url}">
             {route.title}
