f45cae25e310399458044452335db5cfb19d71b5
Folke Sverdrup Nygren <91750365+folkesverdrup@users.noreply.github.com>
2021-10-04 15:44:11 +0200

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,9 +7,11 @@
 
 <main>
     <h1>FOLKE SVERDRUP SAENGER</h1>
-    <p>folke@sverdrupsaenger.com, architect and editor at large</p>
-    
+    <p>folke@sverdrupsaenger.com, 0045 50318037.</p>
     <br/> 
+    <p>twitter/sverdrupsaenger, twitch.tv/erauqs, IG: folkesverdrup and contentbasedlifestyles.</p>
+    <br/>
+    <p>Sign up for the erauqs newsletter at newsletter@erauqs.com when it launches (near future).</p>
     {#each routes as route}
         <a href="/{route.url}">
             {route.title}
