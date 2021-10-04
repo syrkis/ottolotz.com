bf718bedd9d6582e188f77995cab46f7531b71b4
Folke Sverdrup Nygren <91750365+folkesverdrup@users.noreply.github.com>
2021-10-04 15:26:52 +0200

    Update contact.svelte

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,5 +1,5 @@
 <main>
-    email: <a href='mailto:thortao@gmail.com'>thortao@gmail.com</a>
+    email: <a href='mailto:folke.sverdup@outlook.dk'>folke.sverdup@outlook.dk</a>
     <br/> 
-    phone: <a href='tel:+4551918484'>00 45 51 91 84 84</a>
+    phone: <a href='tel:+4551918484'>00 45 50 31 80 37</a>
 </main>
