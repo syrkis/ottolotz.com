9a173d1fe6cf3a86f851887c963f7ca83dfe9dd7
Noah Syrkis <noah@syrkis.com>
2021-11-14 13:50:30 +0100

    test deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,16 +6,5 @@
 </script>
 
 <main>
-    <h1>FOLKE SVERDRUP SAENGER</h1>
-    <p>folke@sverdrupsaenger.com, 0045 50318037.</p>
-    <br/> 
-    <p>twitter/sverdrupsaenger, twitch.tv/erauqs, IG: folkesverdrup and contentbasedlifestyles.</p>
-    <br/>
-    <p>Sign up for the erauqs newsletter at newsletter@erauqs.com when it launches (near future).</p>
-    {#each routes as route}
-        <a href="/{route.url}">
-            {route.title}
-        </a>
-        <br/>
-    {/each}
+    <h1>Otto Lotz</h1>
 </main>
